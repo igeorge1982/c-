f944abbe3b154e0c520a08b73d8037f018a48716
George Gaspar <igeorge1982@gmail.com>
2016-01-14 07:54:45 +0100

    Initial Commit

---
+++ Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Gaspar Gyorgy on 14/01/16.
+//  Copyright © 2016 Gaspar Gyorgy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
